7bbb18256694e904f8bd301778a3404dcb2957e5
rauschma <axel@rauschma.de>
2016-01-17 16:08:33 +0100

    New example: implementing flags via an enum

--- test/basic-tests.js
+++ test/basic-tests.js
@@ -83,3 +83,47 @@ test('Custom prototype method', () => {
     assert.strictEqual(Weekday.SATURDAY.isBusinessDay(), false);
     assert.strictEqual(Weekday.MONDAY.isBusinessDay(), true);
 });
+
+//-------------------------------------------------
+suite('Enum: flags')
+
+class Mode extends Enum {}
+Mode.initEnum({
+    USER_R: {
+        n: 0b100000000,
+    },
+    USER_W: {
+        n: 0b010000000,
+    },
+    USER_X: {
+        n: 0b001000000,
+    },
+    GROUP_R: {
+        n: 0b000100000,
+    },
+    GROUP_W: {
+        n: 0b000010000,
+    },
+    GROUP_X: {
+        n: 0b000001000,
+    },
+    ALL_R: {
+        n: 0b000000100,
+    },
+    ALL_W: {
+        n: 0b000000010,
+    },
+    ALL_X: {
+        n: 0b000000001,
+    },
+});
+test('Using the flags', () => {
+    assert.strictEqual(
+        Mode.USER_R.n | Mode.USER_W.n | Mode.USER_X.n |
+        Mode.GROUP_R.n | Mode.GROUP_X.n |
+        Mode.ALL_R.n | Mode.ALL_X.n,
+        0o755);
+    assert.strictEqual(
+        Mode.USER_R.n | Mode.USER_W.n | Mode.USER_X.n | Mode.GROUP_R.n,
+        0o740);
+});
